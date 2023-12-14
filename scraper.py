4ee95aefc6b838d90a1b03fc729ddbe9e7a70cb4
artin <95845593+artinmohajeri@users.noreply.github.com>
2023-12-14 18:23:04 +0330

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -3,6 +3,7 @@ from bs4 import BeautifulSoup
 import tkinter as tk
 import ttkbootstrap as tb
 from tkinter import ttk
+from tkinter import messagebox
 import sqlite3, time
 
 
@@ -15,8 +16,11 @@ win.minsize(600,350)
 car = None
 home_url = "https://www.autoscout24.com/"
 all_cars = []
-ses = HTMLSession()
-result = ses.get(home_url)
+try:
+    ses = HTMLSession()
+    result = ses.get(home_url)
+except:
+    messagebox.showerror(title="Error", message="Connection Problem")
 doc = BeautifulSoup(result.text, "html.parser")
 cars_menu = doc.find(attrs={"aria-label": "cars-make-filter"})
 cars_menu = cars_menu.find_all("optgroup")
@@ -47,39 +51,40 @@ def click(item):
     car = item
 
 def search():
-    cursor.execute('DELETE FROM cars')
-    session = HTMLSession()
-    param = {"page":1}
-    url = f"https://www.autoscout24.com/lst/{car}?atype=C&desc=0&sort=standard&source=homepage_search-mask&ustate=N%2CU"
-    while True:
-        res = session.get(url, params=param)
-        doc = BeautifulSoup(res.text, "html.parser")
-        names = doc.find_all("h2")
-        models = doc.find_all(class_="ListItem_version__jNjur")
-        prices = doc.find_all(class_="Price_price__WZayw PriceAndSeals_current_price__XscDn")
-        details = doc.find_all(class_="VehicleDetailTable_container__mUUbY")
-        # images = doc.find_all(class_="NewGallery_img__bi92g")
-        next_btn = doc.find(attrs={"aria-label": "Go to next page"})
+    try:
+        cursor.execute('DELETE FROM cars')
+        session = HTMLSession()
+        param = {"page":1}
+        url = f"https://www.autoscout24.com/lst/{car}?atype=C&desc=0&sort=standard&source=homepage_search-mask&ustate=N%2CU"
+        while True:
+            res = session.get(url, params=param)
+            doc = BeautifulSoup(res.text, "html.parser")
+            names = doc.find_all("h2")
+            models = doc.find_all(class_="ListItem_version__jNjur")
+            prices = doc.find_all(class_="Price_price__WZayw PriceAndSeals_current_price__XscDn")
+            details = doc.find_all(class_="VehicleDetailTable_container__mUUbY")
+            # images = doc.find_all(class_="NewGallery_img__bi92g")
+            next_btn = doc.find(attrs={"aria-label": "Go to next page"})
 
-        for name,model,price,detail in zip(names,models,prices,details):
-            name = name.contents[0]
-            model = model.string
-            price = price.string
-            milage = detail.contents[0].contents[1]
-            gearbox = detail.contents[1].contents[1]
-            First_registration = detail.contents[2].contents[1]
-            fuel_type = detail.contents[3].contents[1]
-            power = detail.contents[4].contents[1]
-            cursor.execute('INSERT INTO cars (name, model, price, mileage, gearbox, First_registration, fuel_type, power) VALUES (?,?,?,?,?,?,?,?)', (name, model, price, milage, gearbox, First_registration, fuel_type, power))
+            for name,model,price,detail in zip(names,models,prices,details):
+                name = name.contents[0]
+                model = model.string
+                price = price.string
+                milage = detail.contents[0].contents[1]
+                gearbox = detail.contents[1].contents[1]
+                First_registration = detail.contents[2].contents[1]
+                fuel_type = detail.contents[3].contents[1]
+                power = detail.contents[4].contents[1]
+                cursor.execute('INSERT INTO cars (name, model, price, mileage, gearbox, First_registration, fuel_type, power) VALUES (?,?,?,?,?,?,?,?)', (name, model, price, milage, gearbox, First_registration, fuel_type, power))
 
-        if not next_btn:
-            break
-        param["page"] += 1
-    connect.commit()
-    lab.configure(text="data recieved successfuly!!", fg="lightgreen")
+            if not next_btn:
+                break
+            param["page"] += 1
+        connect.commit()
+        lab.configure(text="data recieved successfuly!!", fg="lightgreen")
 
-    with open("display_database.py","w") as f:
-        f.write('''
+        with open("display.py","w") as f:
+            f.write('''
 
 from tkinter import *
 from tkinter import ttk
@@ -137,7 +142,11 @@ lbl.pack(pady=40)
 
 win.mainloop()
 
-''')
+    ''')
+    
+    except:
+        messagebox.showerror(title="Error", message="Data did NOT recieved")
+        
 
 style.configure('TMenubutton', font=('Helvetica', 14))
 my_menu = tb.Menubutton(win, bootstyle="outline-info", text="choose a car", cursor="hand2")
